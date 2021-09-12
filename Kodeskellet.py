1971600342192e37760a5679c53fdd4f495804b3
EmilieML <74358307+EmilieML@users.noreply.github.com>
2021-09-12 22:46:23 +0200

    Add files via upload

---
+++ Kodeskellet.py
@@ -0,0 +1,24 @@
+import turtle
+import random
+
+turtle.Screen()
+turtle.hideturtle()
+turtle.penup()
+
+
+class Sierpinski:
+
+    def __init__(self):
+        pass
+
+    def triangle(self):
+        pass
+
+    def random_dot(self):
+        pass
+
+    def sierpinski(self):
+        pass
+
+  
+turtle.done()
